b2306fcc38ea7e56b7fa2ffde6cbc25974bd70e0
dumb851 <dumb851@mail.ru>
2017-08-17 15:43:45 +0300

    action bar: done cancel

--- app/src/main/java/com/zubrid/scheduler/ui/ScheduleItemActivity.java
+++ app/src/main/java/com/zubrid/scheduler/ui/ScheduleItemActivity.java
@@ -1,7 +1,12 @@
 package com.zubrid.scheduler.ui;
 
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.zubrid.scheduler.R;
 
@@ -10,6 +15,51 @@ public class ScheduleItemActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setActionBar();
+
         setContentView(R.layout.activity_schedule_item);
     }
+
+    private void setActionBar() {
+
+        //final ActionBar actionBar = getActionBar();
+        final ActionBar actionBar = getSupportActionBar();
+
+        final LayoutInflater inflater = (LayoutInflater) actionBar.getThemedContext()
+                .getSystemService(LAYOUT_INFLATER_SERVICE);
+
+        final View customActionBarView = inflater.inflate(
+                R.layout.actionbar_custom_view_done_cancel, null);
+
+        customActionBarView.findViewById(R.id.actionbar_done).setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // "Done"
+                        finish();
+                    }
+                });
+        customActionBarView.findViewById(R.id.actionbar_cancel).setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // "Cancel"
+                        finish();
+                    }
+                });
+
+        // Show the custom action bar view and hide the normal Home icon and title.
+
+        actionBar.setDisplayOptions(
+                ActionBar.DISPLAY_SHOW_CUSTOM,
+                ActionBar.DISPLAY_SHOW_CUSTOM | ActionBar.DISPLAY_SHOW_HOME
+                        | ActionBar.DISPLAY_SHOW_TITLE);
+        actionBar.setCustomView(customActionBarView,
+                new ActionBar.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.MATCH_PARENT));
+    }
+
+
 }
