35a1cf0688d895378bdd947ceab6e4558a4bc61f
dumb851 <dumb851@mail.ru>
2017-12-26 17:16:32 +0300

    task: slide menu - delete this task

--- app/src/main/java/ui/MainActivityFragment.java
+++ app/src/main/java/ui/MainActivityFragment.java
@@ -1,16 +1,12 @@
 package ui;
 
 import android.content.Intent;
-import android.graphics.Canvas;
-import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
-import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -27,10 +23,6 @@ final public class MainActivityFragment extends Fragment implements ScheduleList
     protected RecyclerView mRecyclerViewScheduleList;
     protected ScheduleListAdapter mScheduleListAdapter;
     protected RecyclerView.LayoutManager mLayoutManager;
-
-    private boolean mSwipeBack;
-
-    private Paint p = new Paint(); //! test block
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -58,12 +50,8 @@ final public class MainActivityFragment extends Fragment implements ScheduleList
             @Override
             public int getMovementFlags(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
 
-                return makeMovementFlags(
-                        ItemTouchHelper.DOWN
-                                | ItemTouchHelper.UP
-                                | ItemTouchHelper.START
-                                | ItemTouchHelper.END,
-                        ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT);
+                return makeFlag(ItemTouchHelper.ACTION_STATE_DRAG,
+                        ItemTouchHelper.DOWN | ItemTouchHelper.UP | ItemTouchHelper.START | ItemTouchHelper.END);
 
             }
 
@@ -98,102 +86,7 @@ final public class MainActivityFragment extends Fragment implements ScheduleList
                     mScheduleListAdapter.unselectItems();
                 }
 
-                mSwipeBack = false;
-
             }
-
-            @Override
-            public int convertToAbsoluteDirection(int flags, int layoutDirection) {
-
-                // TODO: 17.11.2017 selecting works bad, should review
-                // gonna try this
-                // https://www.learn2crack.com/2016/02/custom-swipe-recyclerview.html
-
-                Log.d(TAG, "convertToAbsoluteDirection: ");
-                Log.d(TAG, "convertToAbsoluteDirection: mSwipeBack= " + mSwipeBack);
-
-                int result;
-
-                if (mSwipeBack) {
-                    mSwipeBack = false;
-                    return 0;
-                }
-
-                result = super.convertToAbsoluteDirection(flags, layoutDirection);
-                return result;
-            }
-
-            @Override
-            public void onChildDraw(Canvas c, RecyclerView recyclerView,
-                                    RecyclerView.ViewHolder viewHolder,
-                                    float dX, float dY, int actionState,
-                                    boolean isCurrentlyActive) {
-
-
-                //!test block start
-//                Bitmap icon;
-//                if(actionState == ItemTouchHelper.ACTION_STATE_SWIPE){
-//
-//                    View itemView = viewHolder.itemView;
-//                    float height = (float) itemView.getBottom() - (float) itemView.getTop();
-//                    float width = height / 3;
-//
-//                    if(dX > 0){
-//                        p.setColor(Color.parseColor("#388E3C"));
-//                        RectF background = new RectF((float) itemView.getLeft(), (float) itemView.getTop(), dX,(float) itemView.getBottom());
-//                        c.drawRect(background,p);
-//                        icon = BitmapFactory.decodeResource(getResources(), R.drawable.ic_pause);
-//                        RectF icon_dest = new RectF((float) itemView.getLeft() + width ,(float) itemView.getTop() + width,(float) itemView.getLeft()+ 2*width,(float)itemView.getBottom() - width);
-//                        c.drawBitmap(icon,null,icon_dest,p);
-//                    } else {
-//                        p.setColor(Color.parseColor("#D32F2F"));
-//                        RectF background = new RectF((float) itemView.getRight() + dX, (float) itemView.getTop(),(float) itemView.getRight(), (float) itemView.getBottom());
-//                        c.drawRect(background,p);
-//                        icon = BitmapFactory.decodeResource(getResources(), R.drawable.ic_pause);
-//                        RectF icon_dest = new RectF((float) itemView.getRight() - 2*width ,(float) itemView.getTop() + width,(float) itemView.getRight() - width,(float)itemView.getBottom() - width);
-//                        c.drawBitmap(icon,null,icon_dest,p);
-//                    }
-//                }
-
-                //test block finish
-
-
-
-
-
-                Log.d(TAG, "onChildDraw: actionState = " + actionState);
-
-                if (actionState == ItemTouchHelper.ACTION_STATE_SWIPE) {
-
-                    //mSwipeBack = true;
-
-                    //!
-
-                    recyclerView.setOnTouchListener(new View.OnTouchListener() {
-                        @Override
-                        public boolean onTouch(View v, MotionEvent event) {
-
-                            Log.d(TAG, "onTouch: getAction() = " + event.getAction());
-
-                            if (mSwipeBack) {
-                                mSwipeBack = false;
-
-                            } else {
-                                mSwipeBack = event.getAction() == MotionEvent.ACTION_CANCEL
-                                        || event.getAction() == MotionEvent.ACTION_UP;
-                            }
-
-                            Log.d(TAG, "onTouch: mSwipeBack set = " + mSwipeBack);
-                            return false;
-                        }
-                    });
-                } else {
-                    mSwipeBack = false;
-                }
-
-                super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
-            }
-
 
         };
 
