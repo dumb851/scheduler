5c0c3273c0e54913a0fef1dde80a5a96de9f78c0
dumb851 <dumb851@gmail.com>
2018-03-25 20:55:22 +0300

    refactoring scheduled_timer.xml

--- app/src/main/java/ui/activity/ScheduleItemActivity.java
+++ app/src/main/java/ui/activity/ScheduleItemActivity.java
@@ -58,7 +58,7 @@ public final class ScheduleItemActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.schedule_item);
+        setContentView(R.layout.scheduled_timer);
 
         setToolbar();
 
